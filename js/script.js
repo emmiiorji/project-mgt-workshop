7960dc9df48a152107f8e422de29e53c0e6eae72
emmiiorji <emmanuelorji2013@gmail.com>
2022-04-23 10:02:06 +0100

    Create speakers section dynamically

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
+import speakersData from './speakersData.js';
+
+const speakers = speakersData();
+
+function markupSpeaker(speaker) {
+  return `<div class="speaker-card">
+            <div class="grid-bg"></div>
+            <img src="${speaker.img}" class="speaker-img" alt="${speaker.name}">
+            <div class="speaker-info">
+              <h3 class="speaker-name">${speaker.name}</h3>
+              <p class="speaker-title">${speaker.title}</p>
+              <hr class="sep-info">
+              <p class="speaker-bio">${speaker.bio}</p>
+            </div>
+          </div>`;
+}
+
+function createAllSpeakers() {
+  let speakersMarkup = '';
+  speakers.forEach((speaker) => {
+    speakersMarkup += markupSpeaker(speaker);
+  });
+  return speakersMarkup;
+}
+
 function toggleMobileMenu() {
   document.querySelectorAll('.menu-element, #hamburger').forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('active');
   }));
 }
-
 toggleMobileMenu();
